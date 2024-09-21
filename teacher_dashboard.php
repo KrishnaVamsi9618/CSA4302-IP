0a02a957c01ba40ed86ba70708028fd6d558c846
Krishna Vamsi <121479079+KrishnaVamsi9618@users.noreply.github.com>
2024-09-21 14:54:24 +0530

    Delete teacher_dashboard.php

--- teacher_dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Check if the teacher is logged in
-if (!isset($_SESSION['username']) || $_SESSION['role'] != 'teacher') {
-    header('Location: login.php');
-    exit();
-}
-?>
